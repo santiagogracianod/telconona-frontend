96dcaf85d0f74d08732ccf2042b756f35bd5058b
Santiago Graciano <santiago.gracianod@gmail.com>
2025-06-11 19:56:15 -0500

    feat(ci/cd): independencia de jobs

--- lib/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from "msw/browser"
-import { handlers } from "./handlers"
-
-// Este archivo configura el worker para el navegador
-export const worker = setupWorker(...handlers)
